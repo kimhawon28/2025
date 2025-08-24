dbb310ca501d34e3dc5ebaa0f748611f6f34f241
kimhawon28 <hw_k-abc@naver.com>
2025-08-24 18:15:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -648,7 +648,7 @@ from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 
 
 # 📌 PDF 생성 함수
-def pdf_file = make_monthly_calendar_pdf(year, month, plan_scoped_df):
+pdf_file = make_monthly_calendar_pdf(year, month, plan_scoped_df):
     buffer = BytesIO()
 
     # 한글 폰트 등록
